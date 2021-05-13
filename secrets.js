d34110365c6633c6fa4fc285a6437a7897e66ee1
saranshdhingra <saranshdhingra@google.com>
2021-05-13 17:41:11 +0530

    Fixed a typo in secrets.js

--- secrets.js
+++ secrets.js
@@ -8,7 +8,7 @@ async function accessSecrets(){
         let secrets=['COWIN_DB_HOST','COWIN_DB_PORT','COWIN_DB_USERNAME','COWIN_DB_PASSWORD','COWIN_DB_DATABASE','COWIN_MAILGUN_KEY','COWIN_MAILGUN_DOMAIN','COWIN_SENDER_EMAIL','COWIN_SENDER_NAME','COWIN_MIN_NOTIFY_DELAY_SECS'];
         for(secret of secrets){
             const [version] = await client.accessSecretVersion({
-                name: `projects/${COWIN_PROJECT_ID}/secrets/${secret}/versions/latest`
+                name: `projects/${process.env.COWIN_PROJECT_ID}/secrets/${secret}/versions/latest`
             });
         
             // Extract the payload as a string.

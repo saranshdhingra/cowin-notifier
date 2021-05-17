b7d1c28fa55cbd81d58b48d28d8c7a8f39b65931
saranshdhingra <saranshdhingra@google.com>
2021-05-17 19:40:08 +0530

    Replaced mailgun with sendgrid

--- mail.js
+++ mail.js
@@ -1,28 +1,32 @@
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
+// const formData = require('form-data');
+// const Mailgun = require('mailgun.js');
+// const mailgun = new Mailgun(formData);
+const sgMail = require('@sendgrid/mail')
 let mg;
 
 async function sendMail(email,foundSessions){
     return new Promise((resolve)=>{
-        if(mg===undefined){
-            mailgun.client({username: 'api', key:  process.env.COWIN_MAILGUN_KEY});
+        sgMail.setApiKey(process.env.COWIN_SENDGRID_API_KEY)
+        const msg = {
+        to: email, // Change to your recipient
+        from: `${process.env.COWIN_SENDER_NAME} <${process.env.COWIN_SENDER_EMAIL}>`, // Change to your verified sender
+        subject: 'A slot just opened uup on the Cowin App!',
+        text: foundSessions.join(''),
+        html: foundSessions.join('').split('\n').join('<br>'),
         }
-        
-        mg.messages.create(process.env.MAILGUN_DOMAIN, {
-            from: `${process.env.COWIN_SENDER_NAME} <${process.env.COWIN_SENDER_EMAIL}>`,
-            to: [email],
-            subject: "Cowin Notifier",
-            text: "A slot just opened uup on the Cowin App!",
-            html: foundSessions.join('').split('\n').join('<br>')
-          })
-          .then((msg) => {
-              console.log(msg);
-              resolve();
-            }) // logs response data
-          .catch(err => console.log(err)); // logs any error
+        sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent');
+            resolve();
+        })
+        .catch((error) => {
+            console.error(error)
+        });
     });
 }
+
+
 
 exports.mailer={
     sendMail
--- secrets.js
+++ secrets.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 async function accessSecrets(){
     if(process.env.NODE_ENV==='production'){
-        let secrets=['COWIN_DB_HOST','COWIN_DB_PORT','COWIN_DB_USERNAME','COWIN_DB_PASSWORD','COWIN_DB_DATABASE','COWIN_MAILGUN_KEY','COWIN_MAILGUN_DOMAIN','COWIN_SENDER_EMAIL','COWIN_SENDER_NAME','COWIN_MIN_NOTIFY_DELAY_SECS'];
+        let secrets=['COWIN_DB_HOST','COWIN_DB_PORT','COWIN_DB_USERNAME','COWIN_DB_PASSWORD','COWIN_DB_DATABASE','COWIN_SENDGRID_API_KEY','COWIN_SENDER_EMAIL','COWIN_SENDER_NAME','COWIN_MIN_NOTIFY_DELAY_SECS'];
         for(secret of secrets){
             const [version] = await client.accessSecretVersion({
                 name: `projects/${process.env.COWIN_PROJECT_ID}/secrets/${secret}/versions/latest`
